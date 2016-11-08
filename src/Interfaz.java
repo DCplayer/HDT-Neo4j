ab7a85d52c9371ae998acaad106aaf885463c369
Jose Jo Escobar <jo14343@uvg.edu.gt>
2016-11-07 21:27:18 -0600

    Agregando interfaz grafica con instrucciones de uso

---
+++ src/Interfaz.java
@@ -0,0 +1,112 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Interfaz {
+
+	private JFrame frmVisualizadorDeGrafos;
+	
+	private JTextField txtNinguno;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interfaz window = new Interfaz();
+					window.frmVisualizadorDeGrafos.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Interfaz() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmVisualizadorDeGrafos = new JFrame();
+		frmVisualizadorDeGrafos.setTitle("Visualizador de Grafos");
+		frmVisualizadorDeGrafos.setBounds(100, 100, 450, 300);
+		frmVisualizadorDeGrafos.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmVisualizadorDeGrafos.getContentPane().setLayout(null);
+		
+		JButton btnIncisoA = new JButton("Opcion 1");
+		btnIncisoA.setBounds(10, 77, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoA);
+		
+		JButton btnIncisoB = new JButton("Opcion 2");
+		btnIncisoB.setToolTipText("");
+		btnIncisoB.setBounds(10, 115, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoB);
+		
+		JButton btnIncisoC = new JButton("Opcion 3");
+		btnIncisoC.setBounds(10, 155, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoC);
+		
+		JButton btnIncisoD = new JButton("Opcion 4");
+		btnIncisoD.setBounds(120, 77, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoD);
+		
+		JButton btnIncisoE = new JButton("Opcion 4");
+		btnIncisoE.setBounds(120, 115, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoE);
+		
+		JButton btnIncisoF = new JButton("Opcion 5");
+		btnIncisoF.setBounds(120, 155, 100, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnIncisoF);
+		
+		JLabel lblHojaDeTrabajo = new JLabel("Hoja de Trabajo #10");
+		lblHojaDeTrabajo.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+		lblHojaDeTrabajo.setBounds(10, 11, 291, 44);
+		frmVisualizadorDeGrafos.getContentPane().add(lblHojaDeTrabajo);
+		
+		JButton btnInstrucciones = new JButton("Instrucciones");
+		btnInstrucciones.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Instrucciones info = new Instrucciones();
+				info.setVisible(true);
+			}
+
+		});
+		btnInstrucciones.setBackground(Color.LIGHT_GRAY);
+		btnInstrucciones.setBounds(10, 227, 114, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnInstrucciones);
+		
+		JButton btnCargarArchivo = new JButton("Cargar Archivo ");
+		btnCargarArchivo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		btnCargarArchivo.setBackground(Color.LIGHT_GRAY);
+		btnCargarArchivo.setBounds(274, 190, 150, 23);
+		frmVisualizadorDeGrafos.getContentPane().add(btnCargarArchivo);
+		
+		txtNinguno = new JTextField();
+		txtNinguno.setHorizontalAlignment(SwingConstants.CENTER);
+		txtNinguno.setText("NINGUNO");
+		txtNinguno.setBounds(235, 227, 189, 20);
+		frmVisualizadorDeGrafos.getContentPane().add(txtNinguno);
+		txtNinguno.setColumns(10);
+	}
+}
